01f847f83d7fae9e4dad83bd305ffbe3cd6b7df6
GoreevArtem <goreevartoum@gmail.com>
2023-06-30 03:00:12 +0300

    fix packege

--- frontend/src/app/shared/model/card.model.ts
+++ frontend/src/app/shared/model/card.model.ts
@@ -5,10 +5,17 @@ export class Card {
     public status?: string;
     public photo_name?: string;
     public class_trash?: string;
+    public notViewFlag?: boolean;
+    public viewFlag?: boolean;
+    public cleanFlag?: boolean;
+    public photo_src?:string;
     constructor() {
         this.id=0;
         this.adress="";
         this.request_date="";
         this.status="";
+        this.notViewFlag = false;
+        this.viewFlag = true;
+        this.cleanFlag=false;
      }
 }
